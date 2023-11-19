62bd18a397133e09532335dcefe1b79d9232bce4
omerfisler <100528725+omerfisler@users.noreply.github.com>
2023-11-19 17:33:14 +0300

    1.1 Core Quicksort algorithm

--- main.cpp
+++ main.cpp
@@ -3,25 +3,25 @@
 #include <iostream>
 #include <vector>
 
-template <typename T>
-int quickSortPartition(std::vector<T>& arr, int low, int high) {
-    T pivot = arr[high]; // Choose the last element as the pivot
-    int i = low - 1;
-    for (int j = low; j <= high - 1; ++j) {
-        //in Ascending
-        if (arr[j] <= pivot) {
-            i++;
-            std::swap(arr[i], arr[j]);
-        }
-    }
-    std::swap(arr[i + 1], arr[high]);
-    return (i + 1);
-}
 
 template <typename T>
 void quickSort(std::vector<T>& arr, int low, int high) {
-    if (low < high) {
-        int pivotIndex = quickSortPartition(arr, low, high);
+    
+    if (low < high) 
+    {
+        T pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j <= high - 1; ++j) 
+        {
+        //in Ascending
+        if (arr[j] <= pivot) 
+            {
+            i++;
+            std::swap(arr[i], arr[j]);
+            }
+        }
+        std::swap(arr[i + 1], arr[high]);
+        int pivotIndex = i+1;
 
         quickSort(arr, low, pivotIndex - 1); //Sorting the subarrays
         quickSort(arr, pivotIndex + 1, high);
