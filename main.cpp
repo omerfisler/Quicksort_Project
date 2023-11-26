002fd6c474af7edf1448699a1086ce25e590bba4
omerfisler <100528725+omerfisler@users.noreply.github.com>
2023-11-26 13:05:33 +0300

    Part 2- read data from csv
    the input parameters designed in std::pair ( city: string , population: int)

--- main.cpp
+++ main.cpp
@@ -3,14 +3,19 @@
 #include <iostream>
 #include <vector>
 #include <time.h>
+#include <fstream>
+#include <sstream>
+#include <utility>
 
-template <typename T>
-void insertionSort(std::vector<T>& arr, int low, int high) {
+using namespace std;
+
+
+void insertionSort(std::vector<pair<string,int>>& arr, int low, int high) {
     for (int i = low + 1; i <= high; ++i) {
-        T key = arr[i];
+        pair<string,int> key = arr[i];
         int j = i - 1;
 
-        while (j >= low && arr[j] > key) {
+        while (j >= low && arr[j].second > key.second) {
             arr[j + 1] = arr[j];
             --j;
         }
@@ -31,13 +36,14 @@ int pivotRandom(int low, int high){
     return randomIndex;
 }
 
-template <typename T>
-int pivotMedianOfThree(std::vector<T>& arr, int low, int high){
+int pivotMedianOfThree(vector<pair<string,int>>& arr, int low, int high){
     int randomIndex1 = low + (rand() % (high - low + 1));
     int randomIndex2 = low + (rand() % (high - low + 1));
     int randomIndex3 = low + (rand() % (high - low + 1));
     // finding median of three elements algorithm.
-    T median = std::max(std::min(arr[randomIndex1], arr[randomIndex2]), std::min(std::max(arr[randomIndex1], arr[randomIndex2]), arr[randomIndex3]));
+    pair<string,int> median;
+    // find median by using median formula with min,max functions. Considered the integer values of data. (not string's median)
+    median.second = max(min(arr[randomIndex1].second, arr[randomIndex2].second), min(max(arr[randomIndex1].second, arr[randomIndex2].second), arr[randomIndex3].second));
     if (median == arr[randomIndex1])
         return randomIndex1;
     else if (median == arr[randomIndex2])
@@ -46,13 +52,13 @@ int pivotMedianOfThree(std::vector<T>& arr, int low, int high){
         return randomIndex3;
 }
 
-template <typename T>
-int partition(std::vector<T>& arr, int low, int high) {
-    T pivot = arr[high]; // pivot element <-- last element on array
+
+int partition(std::vector<pair<string,int>>& arr, int low, int high) {
+    pair<string,int> pivot = arr[high]; // pivot element <-- last element on array
     int i = low - 1;
 
     for (int j = low; j <= high - 1; j++) {
-        if (arr[j] <= pivot) {
+        if (arr[j].second <= pivot.second) {
             i++;
             if (i<j) // to avoid swapping same elements (when i=j ==> a[i]=a[j]). and also i can't be greater than j due to algorithm.
             {
@@ -65,8 +71,8 @@ int partition(std::vector<T>& arr, int low, int high) {
     return i + 1;
 }
 
-template <typename T>
-void quickSortHybrid(std::vector<T>& arr, int low, int high, char pivotType, int threshold) {
+
+void quickSortHybrid(std::vector<pair<string,int>>& arr, int low, int high, char pivotType, int threshold) {
     
     if (low < high) 
     {
@@ -99,22 +105,56 @@ void quickSortHybrid(std::vector<T>& arr, int low, int high, char pivotType, int
     }
 }
 
+pair<string, int> parseline(const string& line) {
+    stringstream ss(line);
+    string token1, token2;
+
+    getline(ss, token1, ';');
+    getline(ss, token2);
+
+    int intValue = stoi(token2);
+    //Converting string line to (string,int) pair.
+    return make_pair(token1, intValue);
+}
+
+void readDataFromCSV(const string& filename, vector<pair<string,int>>& arr) {
+    
+    ifstream file(filename);
+
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            // parsed line 
+            arr.push_back(parseline(line));
+        }
+
+        file.close();
+    } else {
+        cout << "Unable to open file: " << filename << endl;
+    }
+
+    // return arr;
+}
+
 int main() {
     srand(time(NULL));
+    string filename="dummyData.csv";
     // Example usage
-    std::vector<int> arr = {12, 4, 5, 6, 7, 3, 1, 15};
-    int n = arr.size();
+    vector<pair<string,int>> data;
+    readDataFromCSV(filename,data);
+    std::vector<int> arr = {2,1,6,9,7,4,3,8,5};
+    int n = data.size();
     //threshold for hybrid quicksort method.
     int k =5;
     char pivotType = 'l';
     std::cout << "Enter pivot strategy (Last Element[l], Random Element[r], Median of 3[m]): ";
     std::cin >> pivotType;
     //example for quicksort function
-    quickSortHybrid(arr, 0, n - 1, pivotType, k);
+    quickSortHybrid(data, 0, n - 1, pivotType, k);
 
-    std::cout << "Sorted array: ";
+    std::cout << "Sorted data: "<<endl;
     for (int i = 0; i < n; i++) {
-        std::cout << arr[i] << " ";
+        cout << "\t"<< data[i].first << "\t\t"<<data[i].second<<endl;
     }
     std::cout << std::endl;
     return 0;
